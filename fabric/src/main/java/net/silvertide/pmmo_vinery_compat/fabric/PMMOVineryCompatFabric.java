199d0bd8bec6556220c018046ce52f88ecfbf8bf
Silvertide7 <natephillips801@gmail.com>
2023-11-28 13:45:01 -0700

    Changed all ids to PMMO Compat, deleted unecessary files. Also added initial mixin from architectury boilerplate

--- fabric/src/main/java/net/silvertide/pmmo_vinery_compat/fabric/PMMOVineryCompatFabric.java
+++ fabric/src/main/java/net/silvertide/pmmo_vinery_compat/fabric/PMMOVineryCompatFabric.java
@@ -2,13 +2,11 @@ package net.silvertide.pmmo_vinery_compat.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.silvertide.pmmo_vinery_compat.PMMOVineryCompat;
-import net.silvertide.pmmo_vinery_compat.fabric.world.NetherVineryBiomeModification;
 
 public class PMMOVineryCompatFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         PMMOVineryCompat.init();
         PMMOVineryCompat.commonSetup();
-        NetherVineryBiomeModification.init();
     }
 }
